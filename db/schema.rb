1de6b05ef006b8ff32e0bee5d4ec8ef6abd7a2db
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2010-07-22 17:07:35 -0500

    test

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100722214613) do
+ActiveRecord::Schema.define(:version => 20100701015313) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
